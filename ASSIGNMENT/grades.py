0d3ba44bda7b67bc2a2f74106efb5bd6404bad74
Grace Olayemi <olayemigrace29@gmail.com>
2026-04-11 18:28:28 +0100

    asignment

--- ASSIGNMENT/grades.py
+++ ASSIGNMENT/grades.py
@@ -1,18 +1,15 @@
 grades = int(input("Enter your grade from 0 - 100: "))
 
-if grades >= 90 or grades <= 100:
+if grades >= 90 and grades <= 100:
     print("A")
 
-elif: 
-    grades <= 89 or grades <= 80
+elif grades <= 89 and grades >= 80:
     print("B")
 
-elif: 
-    grades <= 79 or grades <= 70
+elif grades <= 79 and grades >= 70:
     print("C")
 
-elif: 
-    grades <= 69 or grades <= 60
+elif grades <= 69 and grades >= 60:
     print("D")
 
 else:
