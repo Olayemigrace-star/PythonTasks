1e9da2d805c52f6d4837a290348ecefb50a48986
Grace Olayemi <olayemigrace29@gmail.com>
2026-04-20 10:38:20 +0100

    Morning Task

---
+++ CLASSWORK/multiplicationOfSix.py
@@ -0,0 +1,12 @@
+
+"""
+
+Create a loop from 1 to 10
+and multiply the number by 6
+"""
+
+
+for(int number = 1; number <= 10; number++)
+int product = 6 * number
+print(6 * "number" + "= " + product)
+
